d2b6f2d0b3c9fd7b845f4b382858f169176cef5b
AmarinoLama <amarinolama@gmail.com>
2024-05-23 11:08:14 +0200

    feat (Usuaria) : add attributes, constructor, getters and setters

--- src/main/java/org/acme/dominio/Usuaria.java
+++ src/main/java/org/acme/dominio/Usuaria.java
@@ -1,4 +1,38 @@
 package org.acme.dominio;
 
-public class Usuaria {
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "t_users")
+public class Usuaria extends PanacheEntityBase {
+
+    @Id
+    @Column(name="user_nom")
+    private String user_nom;
+
+    @Column(name="user_prop")
+    private Integer user_prop;
+
+    public Usuaria() {
+    }
+
+    public String getUser_nom() {
+        return user_nom;
+    }
+
+    public void setUser_nom(String user_nom) {
+        this.user_nom = user_nom;
+    }
+
+    public Integer getUser_prop() {
+        return user_prop;
+    }
+
+    public void setUser_prop(Integer user_prop) {
+        this.user_prop = user_prop;
+    }
 }
