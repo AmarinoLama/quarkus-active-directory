2dde483ecc2604b7c3edb1cc15bb43417610753a
AmarinoLama <amarinolama@gmail.com>
2024-05-23 11:05:58 +0200

    feat (Item) : add attributes, constructor, getters and setters

--- src/main/java/org/acme/dominio/Item.java
+++ src/main/java/org/acme/dominio/Item.java
@@ -1,29 +1,50 @@
 package org.acme.dominio;
 
-import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 
-/**
- * Example JPA entity defined as a Panache Entity.
- * An ID field of Long type is provided, if you want to define your own ID field extends <code>PanacheEntityBase</code> instead.
- *
- * This uses the active record pattern, you can also use the repository pattern instead:
- * .
- *
- * Usage (more example on the documentation)
- *
- * {@code
- *     public void doSomething() {
- *         MyEntity entity1 = new MyEntity();
- *         entity1.field = "field-1";
- *         entity1.persist();
- *
- *         List<MyEntity> entities = MyEntity.listAll();
- *     }
- * }
- */
 @Entity
-public class Item extends PanacheEntity {
-    public String field;
+@Table(name = "t_items")
+public class Item extends PanacheEntityBase {
+
+    @Id
+    @Column(name = "item_nom")
+    private String item_nom;
+
+    @Column(name = "item_prop")
+    private Integer item_prop;
+
+    @Column(name = "item_tipo")
+    private String item_tipo;
+
+    public Item() {
+    }
+
+    public String getItem_nom() {
+        return item_nom;
+    }
+
+    public void setItem_nom(String item_nom) {
+        this.item_nom = item_nom;
+    }
+
+    public Integer getItem_prop() {
+        return item_prop;
+    }
+
+    public void setItem_prop(Integer item_prop) {
+        this.item_prop = item_prop;
+    }
+
+    public String getItem_tipo() {
+        return item_tipo;
+    }
+
+    public void setItem_tipo(String item_tipo) {
+        this.item_tipo = item_tipo;
+    }
 }
